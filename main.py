2ad0bfbd8717b6f7889624ab5b8f2109af34b7b7
Aditya Shaw <shaditya499@gmail.com>
2024-05-23 15:46:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import smtplib
 import pandas as pd
 import random
 
+#FILL THE BIRTHDAYS.CSV WITH BIRTHDAY DETAILS
 df = pd.read_csv("birthdays.csv")
 now = dt.datetime.now()
 month = now.month
 date = now.day
 
-my_mail = "shaditya499@gmail.com"
-Password = "ibqa gvsd kehu zfvr "
-with open(f"./letter_templates/letter_{random.randint(1,3)}.txt","r") as template:
+my_mail = "[ADD YOUR EMAIL-ID HERE]"
+Password = "[ADD YOUR GMAIL API PASSWORD HERE]"
+with open(f"./letter_{random.randint(1,3)}.txt","r") as template:
     contents = template.read()
 
 birthday_df = df[(df["month"]==month) & (df["day"]==date)]
